b9019e9a0cafe3e100946d295fbc34997f1b5a22
Kevin Buchholz <buchholz.kevin@gmail.com>
2023-03-27 14:39:41 -0400

    Initial Commit

---
+++ Planted/PlantedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantedApp.swift
+//  Planted
+//
+//  Created by Kevin Buchholz on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PlantedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
