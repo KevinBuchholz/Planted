32bcec4428598b7a3215588b4fa88a2d9f9bd039
Kevin Buchholz <buchholz.kevin@gmail.com>
2023-03-29 15:01:24 -0400

    Further UI Develooment, added custom colors, tweaked the logo

--- Planted/ContentView.swift
+++ Planted/ContentView.swift
@@ -11,24 +11,37 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             VStack{
-                Image("PLNTD LOGO")
-                    .resizable()
-                    .padding()
-                NavigationLink(destination: SuggestedView()){
-                    Text("Suggested View")
+                Image("PlantedLogo2")
+                .resizable()
+                .scaledToFit()
+                .padding(40)
+                .offset(x:0, y: -150)
+               
+                NavigationLink(destination: SuggestedView()) {  Text("Suggest A Plant")
                 }
-                .padding()
+                .buttonStyle(.bordered)
+                .buttonBorderShape(.roundedRectangle(radius: 5))
+                .foregroundColor(.white)
+                .background(CustomColor.LightGreen)
+         }
                 NavigationLink(destination: UserPlantsView()){
                     Text("My Plants")
                 }
+                .buttonStyle(.bordered)
+                .buttonBorderShape(.roundedRectangle(radius: 5))
+                .foregroundColor(.white)
+                .background(CustomColor.LightGreen)
                 .padding()
             }
         }
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
+
+
---
+++ Planted/CustomColors.swift
@@ -0,0 +1,16 @@
+//
+//  CustomColors.swift
+//  Planted
+//
+//  Created by Kevin Buchholz on 3/29/23.
+//
+
+import SwiftUI
+
+struct CustomColor {
+    static let DarkGreen = Color("DarkGreen")
+    static let LightGreen = Color("LightGreen")
+    static let Gray = Color("Gray")
+}
+
+
